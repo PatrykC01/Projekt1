e251a4a5865cbb10c5ec0d3a554fc111e117db8c
patryk <20patryk01c@gmail.com>
2024-03-01 19:45:52 +0100

    Uzupelnienie funkcji PobierzSzczegolyProduktow
    Stworzenie obiektu produkt na podstawie przechwyconych danych - nie
    dziala pobieranie obrazka (prawdopodobnie zla sciezka) i wyszukuje tylko
    jeden produkt

--- Projekt1/Projekt1/Program.cs
+++ Projekt1/Projekt1/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using HtmlAgilityPack;
+using System.Globalization;
 
 namespace Projekt1
 {
@@ -16,8 +17,20 @@ namespace Projekt1
         {
            List<string> linkiDoProduktow = PobierzLinkiDoProduktow("https://www.ceneo.pl/Komputery");
 
-            Console.WriteLine("Znaleziono: " + linkiDoProduktow.Count + "produktow");
+            Console.WriteLine("Znaleziono: " + linkiDoProduktow.Count + " produktow");
             List<Produkt> produkty = PobierzSzczegolyProdutkow(linkiDoProduktow);
+            Export(produkty);
+        }
+
+        static void Export(List<Produkt> produkty)
+        {
+            using (var writer = new StreamWriter("C:\\Users\\20pat\\OneDrive\\Pulpit\\dane.txt"))
+            {
+                foreach (var produkt in produkty)
+                {
+                    writer.WriteLine($"Nazwa: {produkt.Nazwa}, Cena: {produkt.Cena}, Img: {produkt.Obrazek}");
+                }
+            }
         }
 
         static List<Produkt> PobierzSzczegolyProdutkow(List<string> urls)
@@ -26,13 +39,20 @@ namespace Projekt1
             foreach (string url in urls)
             {
                 HtmlDocument document = PobierzDokument(url);
-                var titleXPath = "";
-                var priceXPath = "";
-                var imgUrlXPath = "";
+                var titleXPath = "//*[@id=\"body\"]/div[2]/div/div/article/div/div[1]/div[2]/div[1]/div[1]/h1";
+                var zlpriceXPath = "//*[@id=\"body\"]/div[2]/div/div/article/div/div[2]/div/div/div[1]/span/span/span[1]";
+                var grpriceXPath = "//*[@id=\"body\"]/div[2]/div/div/article/div/div[2]/div/div/div[1]/span/span/span[2]";
+                var imgUrlXPath = "//*[@id=\"product-carousel\"]/div/div[1]/div[1]/a/img";
+
                 Produkt produkt= new Produkt();
                 produkt.Nazwa = document.DocumentNode.SelectSingleNode(titleXPath).InnerText;
-                produkt.Obrazek = document.DocumentNode.SelectSingleNode(imgUrlXPath).InnerText;
-                produkt.Cena = float.Parse(document.DocumentNode.SelectSingleNode(priceXPath).InnerText);
+                /*var ObrazekLink = document.DocumentNode.SelectSingleNode(imgUrlXPath);
+
+                string href = ObrazekLink.Attributes[name: "href"].Value;*/
+
+                /*produkt.Obrazek = href; */
+                var fullPrice = float.Parse ((document.DocumentNode.SelectSingleNode(zlpriceXPath).InnerText) + (document.DocumentNode.SelectSingleNode(grpriceXPath).InnerText));
+                produkt.Cena = fullPrice;
                 produkty.Add(produkt);
             }
 
@@ -49,7 +69,7 @@ namespace Projekt1
         {
             List<string> linkiDoProduktow = new List<string>();
             HtmlDocument doc = PobierzDokument(url);
-            HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes("//div[@class='cat-prod-row js_category-list-item js_clickHashData js_man-track-event']");//uzupełnić ścieżke
+            HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes("//*[@id=\"body\"]/div/div/div[3]/div/section/div[4]/div[2]/div/div[1]/a");//uzupełnić ścieżke
             Uri baseUri = new Uri(url);
 
             foreach (HtmlNode link in linkNodes)
